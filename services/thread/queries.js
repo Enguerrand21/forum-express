f2c465b34e8d2d802bad698b65eef27ff1634cbe
enguerrand21 <jeremylheureux@gmail.com>
2026-02-12 09:45:11 +0100

    premier commit

--- services/thread/queries.js
+++
@@ -1,7 +0,0 @@
-import { Thread } from "../../models/Thread.js";
-
-export async function getThreads(currentPage){
-    const threads = await Thread.find().sort({createAt: -1}).limit(10)
-
-    return {threads}
-}
